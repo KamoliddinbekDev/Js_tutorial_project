eeb0935a8aaa05b570c185b887b4afcf1a8884aa
Kamoliddinbek <xudoyberdiyevkamoliddinbekdev@gmail.com>
2024-02-13 17:15:08 +0500

    Delegatsiya

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 "use strict";
+
+// ----------------------- classList
+
 const btns = document.querySelectorAll("button");
 
-console.log(btns[0].classList.value);
-console.log(btns[0].classList.item(1));
+//console.log(btns[0].classList.value);
+//console.log(btns[0].classList.item(1));
 
 btns[1].classList.add("blue");
 btns[1].classList.remove("blue");
@@ -17,3 +20,23 @@ btns[0].addEventListener("click", () => {
 	btns[1].classList.add("blue");
     }
 });
+
+// ----------------------- delegatsiya
+
+const wrapper = document.querySelector(".btn-block");
+
+wrapper.addEventListener("click", (e) => {
+    // console.log(e.target);
+    if(e.target && e.target.tagName == "BUTTON" && e.target.classList.contains("blue")) {
+	console.log("Blue button clicked");
+    }
+});
+
+btns.forEach(btn => btn.addEventListener("click", () => console.log("button clicked")));
+
+const newBtn = document.createElement("button");
+newBtn.classList.add("blue");
+wrapper.append(newBtn);
+
+//
+
